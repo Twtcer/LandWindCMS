dcace453725ce68e50966af682df8413337d21fd
landwind <517773735@qq.com>
2019-06-21 17:18:00 +0800

    add

---
+++ src/LandWindCMS.Web.Host/Controllers/PayTestController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using LandWindCMS.Controllers;
+using Microsoft.AspNetCore.Mvc; 
+
+namespace LandWindCMS.Web.Host.Controllers
+{ 
+    public class PayTestController : LandWindCMSControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public string NoityUrl(string dh, string ltype)
+        {
+            if (string.IsNullOrEmpty(dh) || string.IsNullOrEmpty(ltype))
+            {
+                return "error";
+            }
+            else
+            {
+
+                return "success";
+            }
+        }
+
+        #region Model
+        public   class PayConfig
+        {
+            public string GatewayUrl { get; set; } = "http://pay.edlm.cn/";
+            public string AppId { get; set; } = "e5c0f80d4060a8db42b1394f9b966725";
+            public string NoityUrlAsync { get; set; } = HttpUtility.UrlEncode( "");
+            public string ReturnUrl { get; set; }
+            public string Money { get; set; } 
+        }
+        #endregion
+    }
+}
